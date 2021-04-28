7e3c303ec0031fc38830020a66922b25fec576ed
James Ridgway <james@jamesridgway.co.uk>
2021-04-28 14:44:12 +0100

    Button onclick

--- src/Buttons.tsx
+++ src/Buttons.tsx
@@ -3,22 +3,26 @@ import { Button, Col, Row } from "react-bootstrap";
 export function Buttons() {
 
   const buttons = [
-    "C", "+-", "%", "/",
+    "C", "±", "%", "/",
     "7", "8", "9", "x",
     "4", "5", "6", "-",
     "1", "2", "3", "+",
     "0", ".", "="
-  ]
+  ];
+
+  function buttonClicked(val: string) {
+    alert(val);
+  }
 
   function generateButtons() {
-    return buttons.map((val) => <Col xs={3}>
-      <Button>{val}</Button>
+    return buttons.map((val) => <Col xs={val === "0" ? 6 : 3} className="my-1">
+      <Button onClick={() => buttonClicked(val)} block>{val}</Button>
     </Col>);
   }
 
   return <>
-    <Row>    
-      { generateButtons() }
+    <Row>
+      {generateButtons()}
     </Row>
   </>;
 }
