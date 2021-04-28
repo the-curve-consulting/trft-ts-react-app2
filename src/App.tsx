86d78aa52a4d468411f16883b3720d66f80f7f61
James Ridgway <james@jamesridgway.co.uk>
2021-04-28 13:40:49 +0100

    Navigation structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,18 @@ import React from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+import { Navigation } from './Navigation';
 
 function App() {
   return (
-    <Container>
-      <Nav
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-      >
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-            </Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <Button>Click Me</Button>
-    </Container>
+    <>
+      <Container fluid>
+        <Navigation />
+      </Container>
+      <Container>
+        <Button>Click Me</Button>
+      </Container>
+    </>
   );
 }
 
